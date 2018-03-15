a8243d594f83b558ea1299e44aa51538694f41af
Tom <Tom@gmail.com>
2018-03-15 09:06:33 +0000

    add save

--- Code/Matlab/cleanData.m
+++ Code/Matlab/cleanData.m
@@ -17,19 +17,35 @@ zero_lat_lng_rows = Data.Begin_Lat == 0 | Data.Begin_Lng == 0 | ...
                Data.End_Lat == 0 | Data.End_Lng == 0;
 Data = Data(~zero_lat_lng_rows, :);
 
+% removing short trips that are less than 0.5k
+too_small_distance_rows = Data.Distance < 0.5;
+Data = Data(~too_small_distance_rows, :);
+
 % removing short trips that take less than a minute 
-to_small_duration_rows = Data.Duration <= 1 | Data.Distance < 0.01;
-Data = Data(~to_small_duration_rows, :);
+too_big_distance_rows = Data.Distance > 200;
+Data = Data(~too_big_distance_rows, :);
 
 % removing trips or longer than 5 hours
-fivehour_duration_rows = Data.Duration > 300;
+fivehour_duration_rows = Data.Duration > 30;
 Data = Data(~fivehour_duration_rows, :);
 
+Data_dir = fullfile(rootDir(),'Data');
+%writetable(Data,fullfile(Data_dir, 'yoBikeDataClean.csv'))
+too_fast_rows = Data.Speed > 30;
+Data = Data(~too_fast_rows, :);
 
-%too_fast_rows = Data.Speed > 30;
-%Data = Data(~too_fast_rows, :);
+histogram2(Data.Duration, Data.Distance, 'FaceColor','flat')
+xlabel('Duration')
+ylabel('Distance')
+zlabel('Number of Trips')
+colorbar;
 
-Ctrs = [0:300];
-Xcts = hist(Data.Duration, Ctrs);
-figure(1)
-bar(Ctrs, Xcts)
+% Ctrs = [0:0.1:20];
+% Xcts = hist(Data.Distance, Ctrs);
+% figure(1)
+% bar(Ctrs, Xcts)
+% 
+% Ctrs = [0:300];
+% Xcts = hist(Data.Duration, Ctrs);
+% figure(2)
+% bar(Ctrs, Xcts)
